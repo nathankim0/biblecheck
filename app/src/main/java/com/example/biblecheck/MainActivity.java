05943d4b28f32644b782e37562424b01527b1d57
Jinyeob Kim <jinyeob07@gmail.com>
2019-06-28 15:27:31 +0900

    ver2, 앱 꺼도 데이터 저장, 초기화 옵션 구현

--- app/src/main/java/com/example/biblecheck/MainActivity.java
+++ app/src/main/java/com/example/biblecheck/MainActivity.java
@@ -2,50 +2,88 @@ package com.example.biblecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Switch[] sw = new Switch[125];
-    private double cnt=0;
-
+    private Boolean a;
+    private int cnt;
+    SharedPreferences pref;
+    SharedPreferences.Editor ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //저장된 cnt있으면 가져오기 없으면 0으로 초기화
+        pref = getSharedPreferences( "pref" , MODE_PRIVATE);
+        ed = pref.edit();
+        cnt=pref.getInt("savedCnt",0);
+
+        //스위치뷰
         for(int i=1;i<=124;i++){
             sw[i]=findViewById(R.id.switch1+i-1);
+            a=pref.getBoolean("state"+i,false);
+            sw[i].setChecked(a);
         }
 
+        //텍스트뷰
         final TextView textView= findViewById(R.id.textView4);
-        textView.setText("이번 달 "+cnt/124*100+"% 읽으셨습니다.");
+        //읽은분량 바로 변경
+        textView.setText("이번 달 "+(double)cnt/124*100+"% 읽으셨습니다.");
 
+        //스위치뷰 리스너
         Switch.OnCheckedChangeListener OnCheckedChangeListener = new Switch.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
+                if(b){ //on시, cnt증가
                     cnt++;
                 }
-                else{
+                else{ //off시, cnt 감소
                     cnt--;
                 }
-                textView.setText("이번 달 "+cnt/124*100+"% 읽으셨습니다.");
+                textView.setText("이번 달 "+(double)cnt/124*100+"% 읽으셨습니다.");
+                //값저장
+                ed.putInt( "savedCnt" , cnt);
+                for(int i=1;i<=124;i++) {
+                    ed.putBoolean("state"+i,sw[i].isChecked());
+                }
+                ed.commit();
             }
         };
 
-for(int i=1;i<=124;i++) {
-    sw[i].setOnCheckedChangeListener(OnCheckedChangeListener);
-}
+        for(int i=1;i<=124;i++) {
+      sw[i].setOnCheckedChangeListener(OnCheckedChangeListener);
+        }
+
     }
     public void onClick(View view){
         Intent intent=new Intent(MainActivity.this,tree.class);
-        intent.putExtra("percent",cnt/124*100);
+        intent.putExtra("percent",(double)cnt/124*100);
         startActivity(intent);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+            ed.clear();		//전체데이터 삭제
+            ed.commit();
+            finish();
+            return true;
     }
 }
